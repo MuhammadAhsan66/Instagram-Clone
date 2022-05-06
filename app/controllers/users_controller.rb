4cd3decba1d2f70ab3e704a6b5a1be2ef6ae8efc
Muhammad-Ahsan-devs <muhammad.ahsan@devsinc.com>
2022-05-06 11:42:12 +0500

    Added user profile page, setup profile image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+
+    if @user == current_user
+      @user.image = 'current_user_pic'
+    else
+      @user.image = 'users_pic'
+    end
   end
 end
