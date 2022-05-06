844daee6a540644cbb4860817cd96d49d5ea6761
Muhammad-Ahsan-devs <muhammad.ahsan@devsinc.com>
2022-05-06 15:40:51 +0500

    Changes Suggested by buddy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
-    @user.image = @user == current_user ? 'current_user_pic' : 'users_pic'
+    begin
+      @user = User.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      render 'usernotfound'
+    else
+      @user.image = @user == current_user ? 'current_user_pic' : 'users_pic'
+    end
   end
 end
