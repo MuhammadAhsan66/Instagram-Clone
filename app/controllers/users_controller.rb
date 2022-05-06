a62f3533e116f2f92f9a47243afabb43a6cb6d7d
Muhammad-Ahsan-devs <muhammad.ahsan@devsinc.com>
2022-05-06 15:06:31 +0500

    Changes Suggested by buddy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-
-    if @user == current_user
-      @user.image = 'current_user_pic'
-    else
-      @user.image = 'users_pic'
-    end
+    @user.image = @user == current_user ? 'current_user_pic' : 'users_pic'
   end
 end
