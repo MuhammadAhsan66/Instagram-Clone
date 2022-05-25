d64b167deb09254074415d5ad30d66ce4fd96097
Muhammad-Ahsan-devs <muhammad.ahsan@devsinc.com>
2022-05-25 19:16:32 +0500

    Post Likes
    *Post Model: Generate Post Model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :photos, dependent: :destroy
+  has_many :likes, -> { order 'created_at desc' }
   validates_length_of :photos, maximum: 10
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   root 'posts#index'
   devise_for :users,
              path: '',
-             path_names: { sign_in: 'log_in', sign_out: 'log_out', edit: 'profile', sign_up: 'registration' }
+             path_names: { sign_in: 'log_in', sign_out: 'log_out', edit: 'profile', sign_up: 'register' }
 
-  resources :users, only: [:show]
+  resources :users, only: %i[show]
   resources :posts do
     resources :photos, only: %i[create]
+    resources :likes, only: %i[create destory]
   end
 end
