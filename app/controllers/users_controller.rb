59be6644886196d4e12c66f3451f4cfe71085772
Muhammad-Ahsan-devs <muhammad.ahsan@devsinc.com>
2022-05-28 04:06:19 +0500

    Search Feature
    User can search any user
    if a user tries to search then the list will be populated below
    upon clicking on the option a user will be redirected to its show page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.search(params[:term])
+    respond_to :js
+  end
+
   def show
     @user = User.find(params[:id])
     @user.image = @user == current_user ? 'current_user_pic' : 'users_pic'
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
              path: '',
              path_names: { sign_in: 'log_in', sign_out: 'log_out', edit: 'profile', sign_up: 'register' }
 
-  resources :users, only: %i[show]
+  resources :users, only: %i[index show]
   resources :posts do
     resources :photos, only: %i[create]
     resources :likes, only: %i[create destroy], shallow: true
