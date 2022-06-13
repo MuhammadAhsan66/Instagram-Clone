325637192a76b0903ac836c58a34b197620ba1c7
Muhammad-Ahsan-devs <muhammad.ahsan@devsinc.com>
2022-06-13 07:59:54 +0500

    Add request feature
    User can send follow request to other user.
    User can accept or reject follow request

---
+++ app/controllers/requests_controller.rb
@@ -0,0 +1,32 @@
+class RequestsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_request, only: %i[edit destroy]
+
+  def index
+    @requests = current_user.follower_relationships.where(status: 'pending')
+  end
+
+  def edit
+    @request.update!(status: 'accepted')
+  rescue ActiveRecord::RecordNotDestroyed => e
+    flash[:alert] = e.record.errors.full_messages
+  else
+    flash[:notice] = 'Request Accepted!'
+    redirect_to requests_path
+  end
+
+  def destroy
+    @request.destroy!
+  rescue ActiveRecord::RecordNotDestroyed => e
+    flash[:alert] = e.record.errors.full_messages
+  else
+    flash[:notice] = 'Request Deleted!'
+    redirect_to requests_path
+  end
+
+  private
+
+  def set_request
+    @request = Follow.find_by(id: params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,5 @@ Rails.application.routes.draw do
     resources :comments, shallow: true
   end
   resources :stories
+  resources :requests, only: %i[index edit destroy]
 end
