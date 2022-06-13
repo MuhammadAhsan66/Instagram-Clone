3cc225abda71d98f04df74ef7ebb1790df8049d3
Muhammad-Ahsan-devs <muhammad.ahsan@devsinc.com>
2022-06-13 14:33:00 +0500

    Rubocop Green Status

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,8 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
   include Pundit::Authorization
-  include Pagy::Backend
-  before_action :configure_permitted_parameters, if: :devise_controller?
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
